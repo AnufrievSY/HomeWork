b5d9acfa252ebe7a7c7c7858347a075e7d06a53c
AnufrievSy <anufrievsy@bk.ru>
2021-10-07 16:09:46 +0500

    update Lesson1.2.py

--- Lesson1.2.py
+++ Lesson1.2.py
@@ -1,7 +1,13 @@
 time = int(input('введите число: '))
 
 hours = time // 3600
+if hours < 10:
+    hours = f'0{hours}'
 minutes = time % 3600 // 60
+if minutes < 10:
+    minutes = f'0{minutes}'
 seconds = time % 3600 % 60
+if seconds < 10:
+    seconds = f'0{seconds}'
 
 print(f'{hours}:{minutes}:{seconds}')
